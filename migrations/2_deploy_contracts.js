2c44aa7454f1ad85e7e67d89cc8d91059e6d8658
Rus Hughes <russell.hughes@gmail.com>
2017-08-14 13:03:03 +0700

    we can compile and migrate the RSPLT contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var RSPLT = artifacts.require("./RSPLT.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(RSPLT);
 };
